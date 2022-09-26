b8ba5d026273204024e8ed8d104136d2da1ced90
Anoop <anoopmanoharan2@gmail.com>
2022-09-26 14:08:24 +0530

    This is my second code push in got

---
+++ test1.py
@@ -0,0 +1,66 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://anoopmanoharan:Idontremember@cluster0.nibrjix.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}
+c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}c = {
+    "name": "anoop",
+   "email": "anoopmanoharan2@gmail.com",
+   "surname": "manoharan"
+}
+
+
+
+
+
+
+
+
+
+
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(c)
